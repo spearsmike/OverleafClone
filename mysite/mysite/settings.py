9d3be81fce17e96f34750fa3b013deef213e4780
Michael <mspears@mail.csuchico.edu>
2020-12-18 04:35:32 +0000

    Fix allowed hosts

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jo1yc$nkcg^9he!*mf+tumj5hftxro(28@c!hvoa#8uu@yt)%d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["35.209.128.134"]
 
 LOGIN_REDIRECT_URL="/"
 LOGIN_URL="/login/"
